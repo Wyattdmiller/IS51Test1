fa5b761d57cfe7d8c80db92524db550d726eb42c
Wyattdmiller <Wyatt@MacBook-Pro.local>
2020-11-12 16:36:58 -0800

    Committing pseudo-code

--- test1file.py
+++ test1file.py
@@ -6,3 +6,28 @@ with the amount doubling each day. The functions will determine the final pay fo
 Finally, the final pays of each option will be compared, and the program will display the 
 salary option that pays the most, or it will say that they pay the same.
 """
+
+""" Pseudo-code
+Option 1: 
+  pay = 0 
+  day = 0
+  pay += 100
+  loop 10 times
+  return amount
+
+Option 2:
+  pay = 1
+  day = 0
+  total_pay = 0
+  pay *= 2
+  total_pay += pay
+  Loop 10 times
+  return amount
+
+Main:
+  var1 = option1
+  var2 = option2
+  if the amounts are the same, output to the user, "Option 1 and Option 2 pay the same"
+  if option 1 is better, output to the user, "Option 1 is better."
+  if option 2 is better, output to the user, "Option 2 is better."
+"""
