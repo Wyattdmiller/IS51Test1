0e65b54481413db027d22d228419573737dbe16b
Wyattdmiller <Wyatt@MacBook-Pro.local>
2020-11-12 16:26:21 -0800

    committing structured-english

---
+++ test1file.py
@@ -0,0 +1,8 @@
+""" Structured English
+This program will create two functions. Each function evaluates a different salary option.
+They will calculate the pay for each day. The first function adds $100 per day.
+The second function adds $1 the first day, $2 the second day, $4 the third day, and so on, 
+with the amount doubling each day. The functions will determine the final pay for each option.
+Finally, the final pays of each option will be compared, and the program will display the 
+salary option that pays the most, or it will say that they pay the same.
+"""
